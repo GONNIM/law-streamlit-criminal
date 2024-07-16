46a9424568c7122419ab99b1d5d345398e831e07
GONNIM <suauncle@gmail.com>
2024-07-16 17:30:08 +0900

    Modified Criminal Chatbot App - llm.py > get_retriever(), index_name

--- llm.py
+++ llm.py
@@ -24,7 +24,7 @@ def get_session_history(session_id: str) -> BaseChatMessageHistory:
 
 def get_retriever():
     embedding = OpenAIEmbeddings(model='text-embedding-3-large')
-    index_name = 'civil-markdown-index'
+    index_name = 'criminal-markdown-index'
     database = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=embedding)
     retriever = database.as_retriever(search_kwargs={'k': 4})
     return retriever
